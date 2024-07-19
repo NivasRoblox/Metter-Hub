fad9904aa65d26f19e18e9416069018bb6166e94
m.better <152856266+NivasRoblox@users.noreply.github.com>
2024-07-19 08:05:29 +0700

    Update BedWars.lua

--- BedWars.lua
+++ BedWars.lua
@@ -99,62 +99,6 @@ if Value == "Execute" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/main/NewMainScript.lua", true))()
 	end    
 })
-
-Universal:AddSlider({
-	Name = "Walkspeed",
-	Min = 20,
-	Max = 1000,
-	Default = 20,
-	Color = Color3.fromRGB(140, 185, 255),
-	Increment = 1,
-	ValueName = "WS",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-Walkspeed = Value
-	end    
-})
-
-Universal:AddToggle({
-	Name = "Keep Walkspeed",
-	Default = false,
-	Callback = function(Value)
-KeepWalkspeed = Value
-            while KeepWalkspeed do
-                if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") ~= nil and game.Players.LocalPlayer.Character.Humanoid.WalkSpeed ~= Walkspeed then
-                    game.Players.LocalPlayer.Character:FindFirstChild("Humanoid").WalkSpeed = Walkspeed
-                end
-task.wait()
-            end
-	end    
-})
-
-Universal:AddSlider({
-	Name = "Jumppower",
-	Min = 50,
-	Max = 1000,
-	Default = 50,
-	Color = Color3.fromRGB(255, 185, 140),
-	Increment = 1,
-	ValueName = "JP",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-Jumppower = Value
-	end    
-})
-
-Universal:AddToggle({
-	Name = "Keep Jumppower",
-	Default = false,
-	Callback = function(Value)
-KeepJumppower = Value
-            while KeepJumppower do
-                if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") ~= nil and game.Players.LocalPlayer.Character.Humanoid.WalkSpeed ~= Jumppower then
-                    game.Players.LocalPlayer.Character:FindFirstChild("Humanoid").WalkSpeed = Jumppower
-                end
-task.wait()
-            end
-	end    
-})
 end
 for i,v in pairs(gethui().Orion:GetDescendants()) do
                     if v.ClassName == "Frame" and v.BackgroundTransparency < 0.3 then
